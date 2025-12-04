71316cfa59eac9140be7ea5e2526d97374e7c035
titistunis2 <titistunis2@gmail.com>
2025-12-04 10:43:22 +0200

    bbbbbbbbbbbbbbbbbbbbb

--- Fitali_API/Controllers/SelectController.cs
+++ Fitali_API/Controllers/SelectController.cs
@@ -17,7 +17,7 @@ namespace Fitali_API.Controllers
             GenderList genders=db.SelectAll();
             return genders;
         }
-
+        [HttpPost]
         public int InsertAGender([FromBody] Gender gender)
         {
             GenderDB db= new GenderDB();
@@ -25,7 +25,15 @@ namespace Fitali_API.Controllers
             int x = db.SaveChanges();
             return x;
         }
-
+        [HttpDelete("{id}")]
+        public int DeleteAGender(int id)
+        {
+            Gender gender = GenderDB.SelectById(id);
+            GenderDB db = new GenderDB();
+            db.Delete(gender);
+            int x = db.SaveChanges();
+            return x;
+        }
 
         [HttpGet]
         [ActionName("PersonSelector")]
@@ -36,9 +44,25 @@ namespace Fitali_API.Controllers
             PersonList persons = db.SelectAll();
             return persons;
         }
+        [HttpPost]
+        public int InsertAPerson(Person p)
+        {
+            PersonDB db = new PersonDB();
+            db.Insert(p);
+            int x = db.SaveChanges();
+            return x;
+        }
+        public int DeleteAPerson(int id)
+        {
+            Person person = PersonDB.SelectById(id);
+            PersonDB db = new PersonDB();
+            db.Delete(person);
+            int x = db.SaveChanges();
+            return x;
+        }
+
         [HttpGet]
         [ActionName("SubscriptionSelector")]
-
         public SubscriptionList SelectAllSubscription()
         {
             SubscriptionDB db = new SubscriptionDB();
@@ -48,6 +72,13 @@ namespace Fitali_API.Controllers
         [HttpGet]
         [ActionName("TrainerSelector")]
 
+        public int InsertASubscription(Subscription s)
+        {
+            SubscriptionDB db = new SubscriptionDB();
+            db.Insert(s);
+            int x = db.SaveChanges();
+            return x;
+        } 
         public TrainerList SelectAllTrainer()
         {
             TrainerDB db = new TrainerDB();
@@ -57,6 +88,13 @@ namespace Fitali_API.Controllers
         [HttpGet]
         [ActionName("TraineeSelector")]
 
+        public int InsertATrainer(Trainer t)
+        {
+            TrainerDB db = new TrainerDB();
+            db.Insert(t);
+            int x = db.SaveChanges();
+            return x;
+        }
         public TraineeList SelectAllTrainee()
         {
             TraineeDB db = new TraineeDB();
@@ -66,6 +104,13 @@ namespace Fitali_API.Controllers
         [HttpGet]
         [ActionName("Training_registrationSelector")]
 
+        public int InsertATrainee(Trainee te)
+        {
+            TraineeDB db = new TraineeDB();
+            db.Insert(te);
+            int x = db.SaveChanges();
+            return x;
+        }
         public Training_registrationList SelectAllTraining_registration()
         {
             Training_registrationDB db = new Training_registrationDB();
@@ -73,6 +118,13 @@ namespace Fitali_API.Controllers
             return training_registrations;
         }
 
+        public int InsertATraining_registration(Training_registration tr)
+        {
+            Training_registrationDB db = new Training_registrationDB();
+            db.Insert(tr);
+            int x = db.SaveChanges();
+            return x;
+        }
         [HttpGet]
         [ActionName("Workouts_of_trainersSelector")]
 
@@ -81,6 +133,13 @@ namespace Fitali_API.Controllers
             Workouts_of_trainersDB db = new Workouts_of_trainersDB();
             Workouts_of_trainersList workouts_of_trainerss = db.SelectAll();
             return workouts_of_trainerss;
+        }
+        public int InsertAWorkouts_of_trainers(Workouts_of_trainers w)
+        {
+            Workouts_of_trainersDB db = new Workouts_of_trainersDB();
+            db.Insert(w);
+            int x = db.SaveChanges();
+            return x;
         }
 
         [HttpGet]
@@ -93,6 +152,13 @@ namespace Fitali_API.Controllers
             return kinds_of_workoutss;
         }
 
+        public int InsertAKinds_of_workouts(Kinds_of_workouts kw)
+        {
+            Kinds_of_workoutsDB db = new Kinds_of_workoutsDB();
+            db.Insert(kw);
+            int x = db.SaveChanges();
+            return x;
+        }
         [HttpGet]
         [ActionName("List_of_Exc_workoutsSelector")]
 
@@ -102,7 +168,13 @@ namespace Fitali_API.Controllers
             List_of_Exc_workoutsList list_of_Exc_workoutss = db.SelectAll();
             return list_of_Exc_workoutss;
         }
-
+        public int InsertAList_of_Exc_workouts(List_of_Exc_workouts l)
+        {
+            List_of_Exc_workoutsDB db = new List_of_Exc_workoutsDB();
+            db.Insert(l);
+            int x = db.SaveChanges();
+            return x;
+        }
 
 
 
